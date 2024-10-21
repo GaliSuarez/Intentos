10d6253b868c208d9d4a2287fb25d7f98f6865a5
GaliSuarez <2030944e@umich.mx>
2024-10-20 19:30:31 -0600

    checar que tasa de interes funcione

---
+++ FuncionesAnualidadVencida (4).R
@@ -0,0 +1,62 @@
+# Se presentan las funciones de interés simple
+# Autor: Karla Galilea Suárez Esquivel
+# V 1.0: 19 de octubre de 2024
+
+# Valor futuro con formula de Anualidades
+ValorFuturoT=function(A,r,Tp){
+                              xVFt=A*((((1+r)^Tp)-1)/r)
+                              return(xVFt)
+                             }
+# Anualidad con formula de Anualidades
+Anualidad=function(VFt,r,Tp){
+                             xA=VFt/((((1+r)^Tp)-1)/r)
+                             return(xA)
+                            }
+
+# Numero de plazos
+Plazos=function(VFt,r,A){
+                        Tp=log(((VFt*r)/A)+1)/log(1+r)
+                        xTp=round(Tp,0)
+                        return(xTp)
+                       }
+
+# Tasa del periodo
+TasaPer=function(VFt,A,Tp){
+                          ecuacion = function(r) {
+                                                   LadoIzquierdo = VFt / A
+                                                   LadoDerecho = ((1 + r) ^ Tp - 1) / r
+                                                   return(LadoIzquierdo - LadoDerecho)
+                                                 }
+                          
+                          # Usar la función uniroot para encontrar la raíz de la ecuación
+                          resultado = uniroot(ecuacion, lower = 0, upper = 1)
+                          
+                          # Mostrar el resultado
+                          r= resultado$root
+            
+}
+
+# Valor actual
+ValorActualT=function(Ava,rVA,TpVA){
+                              VAt=Ava*((1-(1+rVA)^-TpVA)/rVA)
+                              xVAt=round(VAt,0)
+                              return(xVAt)
+                             }
+# Anualidad con valor actual
+AnualidadVAt=function(VAt,rVA,TpVA){
+                                xAva=VAt/((1-(1+rVA)^-TpVA)/rVA)
+                                return(xAva)
+                               }
+# Plazos con valor actual
+PlazosVA=function(VAt,rVA,Ava){
+                           TpVA=-log(1-((VAt*rVA)/Ava))/log(1+rVA)
+                           xTpVA=round(TpVA,0)
+                           return(xTpVA)
+                          }
+
+# Tasa de periodo con Valor Actual
+TasaPerVA=function(VAt,TpVA,Ava){
+                                 tasaObjt=tasaAnualidadVencidaVA(VAt=va,Ava=a,TpVA=tp,umbral=10*(10^-17))
+                                 return(tasaObjt)
+}
+
